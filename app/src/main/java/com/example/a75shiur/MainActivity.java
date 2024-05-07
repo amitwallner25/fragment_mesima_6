ad0f72d3289b5d576e477cb9e6d35b8ec399f211
Amit <va9692@eh.amalnet.k12.il>
2024-05-07 10:16:37 +0300

    added fragment 1 to main

--- app/src/main/java/com/example/a75shiur/MainActivity.java
+++ app/src/main/java/com/example/a75shiur/MainActivity.java
@@ -10,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirstFragment firstfragment = FirstFragment.newInstance();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentView, firstfragment).commit();
     }
 }
